01e6a5bb6f10deb0956c98ba5d03bf1a30f4e9ac
Hemalatha Marikumar <hemalatha.marikumar@syncfusion.com>
2020-08-31 12:33:26 +0530

    Sample included

---
+++ GettingStartedSample/GettingStartedSample/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Syncfusion.RangeNavigator.XForms;
+using Syncfusion.SfChart.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GettingStartedSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+         
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                RangeNavigator.HeightRequest = 150;
+            }
+            else if (Device.RuntimePlatform == Device.iOS)
+            {
+                RangeNavigator.HeightRequest = 100;
+            }
+            else if (Device.RuntimePlatform == Device.UWP && Device.Idiom == TargetIdiom.Phone)
+            {
+                chart.Margin = new Thickness(5);
+                RangeNavigator.Margin = new Thickness(5);
+                RangeNavigator.Intervals = Syncfusion.RangeNavigator.XForms.DateTimeIntervalType.Quarter |
+                                           Syncfusion.RangeNavigator.XForms.DateTimeIntervalType.Month;
+                RangeNavigator.TooltipFormat = "dd/MM/yy";
+                RangeNavigator.LeftTooltipStyle = new TooltipStyle()
+                {
+                    FontSize = 10
+                };
+                RangeNavigator.RightTooltipStyle = new TooltipStyle()
+                {
+                    FontSize = 10
+                };
+            }
+        }
+
+        void nac_RangeChanged(object sender, Syncfusion.RangeNavigator.XForms.RangeChangedEventArgs e)
+        {
+            dateTimeAxis.Minimum = e.ViewRangeStartDate;
+            dateTimeAxis.Maximum = e.ViewRangeEndDate;
+        }
+    }
+
+}
---
+++ GettingStartedSample/GettingStartedSample/ViewModel/ViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Syncfusion.SfChart.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace GettingStartedSample.ViewModel
+{
+	public class ViewModel
+	{
+		public ObservableCollection<ChartDataPoint> DateTimeRangeData { get; set; }
+
+		public ViewModel()
+		{
+			DateTimeRangeData = new ObservableCollection<ChartDataPoint>
+			{
+				new ChartDataPoint(new DateTime(2015, 01, 1), 14),
+				new ChartDataPoint(new DateTime(2015, 02, 1), 54),
+				new ChartDataPoint(new DateTime(2015, 03, 1), 23),
+				new ChartDataPoint(new DateTime(2015, 04, 1), 53),
+				new ChartDataPoint(new DateTime(2015, 05, 1), 25),
+				new ChartDataPoint(new DateTime(2015, 06, 1), 32),
+				new ChartDataPoint(new DateTime(2015, 07, 1), 78),
+				new ChartDataPoint(new DateTime(2015, 08, 1), 100),
+				new ChartDataPoint(new DateTime(2015, 09, 1), 55),
+				new ChartDataPoint(new DateTime(2015, 10, 1), 38),
+				new ChartDataPoint(new DateTime(2015, 11, 1), 27),
+				new ChartDataPoint(new DateTime(2015, 12, 1), 56),
+				new ChartDataPoint(new DateTime(2015, 12, 31), 35)
+			};
+		}
+	}
+
+}
